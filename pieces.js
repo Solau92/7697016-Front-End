2604946158a7f581c41625d44c3bf677df3b37f6
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-17 16:07:49 +0100

    Listes pièces (avec map)

--- pieces.js
+++ pieces.js
@@ -2,6 +2,7 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
+// Parcours de la liste des pièces 
 for (let i = 0; i < pieces.length; i++) {
 
     const article = pieces[i];
@@ -36,6 +37,7 @@ for (let i = 0; i < pieces.length; i++) {
 
  }
 
+ // Boutons trier 
 const boutonTrierCroissant = document.querySelector(".btn-trier-croissant");
 boutonTrierCroissant.addEventListener("click", function () {
     const piecesOrdonneesCroissant = Array.from(pieces);
@@ -54,6 +56,7 @@ boutonTrierDecroissant.addEventListener("click", function () {
     console.log(piecesOrdonneesDeroissant);
 });
 
+ // Boutons filtrer 
 const boutonFiltrerAbordable = document.querySelector(".btn-filtrer-abordable");
 boutonFiltrerAbordable.addEventListener("click", function () {
    const piecesFiltreesAbordable = pieces.filter(function (piece) {
@@ -69,3 +72,46 @@ boutonFiltrerDescription.addEventListener("click", function () {
    });
    console.log(piecesFiltreesDescription);
 });
+
+// Liste des noms des pièces abordables
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+
+//Création de l'élement html liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements)
+
+
+// Liste des noms et prix des pièces disponibles
+const nomsDisponibles = pieces.map(piece => piece.nom);
+const prixDisponibles = pieces.map(piece => piece.prix);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(! pieces[i].disponibilite){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
+    }
+}
+
+//Création de l'élement html liste
+const disponiblesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < prixDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`;
+    disponiblesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.disponibles')
+    .appendChild(disponiblesElements)
