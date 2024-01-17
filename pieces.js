811225c5b4f4aca0a82813234c97d2ba7985bab9
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-17 18:25:51 +0100

    Récupérer les pièces depuis l'API

--- pieces.js
+++ pieces.js
@@ -1,7 +1,9 @@
 import { ajoutListenersAvis } from "./avis.js";
 
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+// Récupération des pièces depuis le fichier Json local
+// const reponse = await fetch("pieces-autos.json");
+// Récupération des pièces depuis l'API
+const reponse = await fetch('http://localhost:8081/pieces/');
 const pieces = await reponse.json();
 
 // Fonction générer pièces 
