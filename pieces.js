0ff339baa804139230bffdcc287a4a902bf9b254
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-17 17:54:47 +0100

    Récupérer les avis depuis l'API

--- pieces.js
+++ pieces.js
@@ -1,3 +1,5 @@
+import { ajoutListenersAvis } from "./avis.js";
+
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
@@ -24,6 +26,9 @@ function genererPieces(pieces) {
         descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
         const stockElement = document.createElement("p");
         stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+        const avisBouton = document.createElement("button");
+        avisBouton.dataset.id = article.id;
+        avisBouton.textContent = "Afficher les avis";
 
         // On rattache la balise article a la section Fiches
         sectionFiches.appendChild(pieceElement);
@@ -32,10 +37,11 @@ function genererPieces(pieces) {
         pieceElement.appendChild(nomElement);
         pieceElement.appendChild(prixElement);
         pieceElement.appendChild(categorieElement);
-        //Ajout des éléments au DOM pour l'exercice
         pieceElement.appendChild(descriptionElement);
         pieceElement.appendChild(stockElement);
+        pieceElement.appendChild(avisBouton);
     }
+    ajoutListenersAvis();
 }
 
 // Affichage initial de la page
